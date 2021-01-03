8003d10b96a70d310db2eaae1f7c3ca084380be1
Josh Soref <jsoref@users.noreply.github.com>
2021-01-03 14:49:48 -0500

    spelling: environment
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/main/Main.ts
+++ src/main/Main.ts
@@ -164,7 +164,7 @@ export function main(init: Init): void {
           localeCode: localeCode,
           exePath: app.getPath('exe'),
           acceptRemote: !!init.args['host-remote'],
-          version: app.getVersion(), // @TODO Manually load this from the package.json file while in a dev enviroment (so it doesn't use Electron's version)
+          version: app.getVersion(), // @TODO Manually load this from the package.json file while in a dev environment (so it doesn't use Electron's version)
         };
         state.backProc.send(JSON.stringify(msg));
       }));
